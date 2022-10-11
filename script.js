1398d2857aa0d5712bbb2aff95045791173a82ac
v <83017202+imightknoiw@users.noreply.github.com>
2022-10-11 19:05:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function run(){
     var w = window.open("about:blank", "_blank", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999,height=9999")
     w.document.write('<script>document.title = "about:blank";</script>' +
 '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 9999;height: 100%;" src="'+url+'"></iframe>');
-}
+  }
 }
