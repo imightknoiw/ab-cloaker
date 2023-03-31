e288c94f4af5cd2f8d9334ba1d6f72cfbfcafe4f
alt <83017202+imightknoiw@users.noreply.github.com>
2023-03-31 07:42:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function run(){
   var url = document.getElementById('url-input').value;
   if (url.length > 7){
-    var w = window.open("about:blank", "_blank", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999,height=9999")
+    var w = window.open("about:blank", "_self", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999,height=9999")
     w.document.write('<script>document.title = "about:blank";</script>' +
 '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 9999;height: 100%;" src="'+url+'"></iframe>');
   }
