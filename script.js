d5d9b0cc3024e0e1f9a529e113a595dad1276c9b
alt <83017202+imightknoiw@users.noreply.github.com>
2023-03-31 08:04:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function run(){
   var url = document.getElementById('url-input').value;
   if (url.length > 7){
-    var w = window.open("about:blank", "_self", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999,height=9999")
-    w.document.write('<script>document.title = "about:blank";</script>' +
-'<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 9999;height: 100%;" src="'+url+'"></iframe>');
+    var w = window.open("about:blank", "_blank", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999,height=9999");
+    w.document.write(`<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none;" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="${url}"></iframe>`);
   }
 }
